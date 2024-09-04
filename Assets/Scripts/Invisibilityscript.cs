92cdb7580ccda92b4b27d28ec5d65e34a7e81c17
AnthonyJRusso <russoanthony621@gmail.com>
2024-09-04 18:05:12 -0400

    changed script

--- Assets/Scripts/Invisibilityscript.cs
+++ Assets/Scripts/Invisibilityscript.cs
@@ -7,28 +7,31 @@ public class Invisibilityscript : MonoBehaviour
     //Drag in object with sprite renderer you want to turn invisible
     public SpriteRenderer spr;
 
-    Collider2D colldr;
-
     //Set time of invisibility
     public float invisTime = 2f;
 
+    //Array of walls the player will be able to pass through during invisibility
+    public GameObject[] invisWalls;
+
+    //Starts timer for length of invisibility
     private bool timerOn;
 
     void Start()
     {
         spr = GetComponent<SpriteRenderer>();
-        colldr = GetComponent<Collider2D>();
     }
 
     void Update()
     {   
         //Spacebar to go invisible
         if(Input.GetKeyDown(KeyCode.Space)){
-
+            //Turns the colliders of all walls placed in array to triggers, allowing player to pass through them
+            foreach(GameObject i in invisWalls){
+                Collider2D colldr = i.GetComponent<Collider2D>();
+                colldr.isTrigger = true;
+            }
             //Player sprite reduces opacity
             spr.color = new Color(1, 1, 1, 0.01f);
-            //Checks player collider as trigger
-            colldr.isTrigger = true;
             //Starts time limit
             timerOn = true;
         }
@@ -40,31 +43,16 @@ public class Invisibilityscript : MonoBehaviour
             if(invisTime <= 0){
                 //Player sprite regains its opacity
                 spr.color = new Color(1, 1, 1, 1f);
-                //Player collider is no longer a trigger
-                colldr.isTrigger = false;
                 //Ends time limit
                 timerOn = false;
                 //Resets time 
                 invisTime = 2f;
+                //Turns the colliders of walls in the array back to normal
+                foreach(GameObject i in invisWalls){
+                Collider2D colldr = i.GetComponent<Collider2D>();
+                colldr.isTrigger = false;
+                }
             }
         } 
-    }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {   
-        //Checks if object has Sidescript added to it. Add the Sidescript to anything the player should never be able to pass through even when invisible
-        Sidescript side = collision.gameObject.GetComponent<Sidescript>();
-
-        if(side){
-            
-            //Player sprite regains its opacity
-            spr.color = new Color(1, 1, 1, 1f);
-            //Player collider is no longer a trigger
-            colldr.isTrigger = false;
-            //Ends time limit
-            timerOn = false;
-            //Resets time 
-            invisTime = 2f;
-        }
     }
 }
